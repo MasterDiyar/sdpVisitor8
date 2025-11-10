4ffefa20602ec51baeeef2bd4ff8bb03ab8123fd
readmaoo <you@example.com>
2025-11-10 13:48:37 +0500

    alpha v0.1.2a

---
+++ scripts/factories/MountainScene.cs
@@ -0,0 +1,16 @@
+﻿namespace finalSDP.scripts.factories;
+using Godot;
+
+public partial class MountainScene : Node2D
+{
+
+    public override void _Ready()
+    {
+        var camera = GetNode<Camera2D>("Camera2D");
+
+        camera.LimitLeft = 0;
+        camera.LimitRight = 2550;
+        camera.LimitTop = 0;
+        camera.LimitBottom = 825;
+    }
+}
